28d8577852cf1f80f0f5c6c32b83541baed61a31
Unai <unai.abrisqueta@gmail.com>
2016-05-23 23:12:31 +0200

    Add some instruction comments to iTrem2-ssh.zsh

--- iTrem2-ssh.zsh
+++ iTrem2-ssh.zsh
@@ -1,11 +1,11 @@
 function tabc() {
+  # If you have trouble with this, change "Default" to the name of your default theme
   NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi
   echo -e "\033]50;SetProfile=$NAME\a"
 }
 
 function tab-reset() {
-  # If you have trouble with this, change
-  # "Default" to the name of your default theme
+  # If you have trouble with this, change "Default" to the name of your default theme
   NAME="Default"
   echo -e "\033]50;SetProfile=$NAME\a"
 }
@@ -13,6 +13,7 @@ function tab-reset() {
 function colorssh() {
   if [[ -n "$ITERM_SESSION_ID" ]]; then
     trap "tab-reset" INT EXIT
+    # Here you can add or remove all "<name>@*" you want.
     if [[ "$*" =~ "root@*|ubuntu@*" ]]; then
       tabc SSH
     fi
