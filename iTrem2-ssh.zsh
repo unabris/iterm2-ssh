c84762e32cbac4d0a2eb696d63454cf98ea427fe
Unai <unai.abrisqueta@gmail.com>
2016-05-23 23:05:18 +0200

    Add theme change functionality to iTrem2-ssh.zsh

--- iTrem2-ssh.zsh
+++ iTrem2-ssh.zsh
@@ -1,1 +1,25 @@
+function tabc() {
+  NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi
+  echo -e "\033]50;SetProfile=$NAME\a"
+}
 
+function tab-reset() {
+  # If you have trouble with this, change
+  # "Default" to the name of your default theme
+  NAME="Default"
+  echo -e "\033]50;SetProfile=$NAME\a"
+}
+
+function colorssh() {
+  if [[ -n "$ITERM_SESSION_ID" ]]; then
+    trap "tab-reset" INT EXIT
+    if [[ "$*" =~ "root@*|ubuntu@*" ]]; then
+      tabc SSH
+    fi
+  fi
+  ssh $*
+}
+
+compdef _ssh tabc=ssh
+
+alias ssh="colorssh"
